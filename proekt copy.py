45b866cfb940d41955132b83947e2c6417455b17
Pavellko <pavelkollegov@gmail.com>
2022-05-20 16:08:30 +0300

    123

---
+++ proekt copy.py
@@ -0,0 +1,26 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import  QProgressBar, QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, QHBoxLayout, QVBoxLayout, QFormLayout
+from PyQt5.QtGui import QPixmap
+
+app = QApplication([])
+ui = uic.loadUi("interfeis2.ui")
+
+ui.setWindowTitle('Моя программа')
+ui.setFixedSize(1150, 780)
+
+qp = QPixmap('int2.jpg')
+# qp2 = QPixmap('logo1.png')
+ui.label.setPixmap(qp)
+# ui.label_3.setPixmap(qp2)
+ui.show()
+
+# def inp():
+#     text = ui.lineEdit.text()
+#     ui.progressBar.setValue(100)
+#     ui.label_2.setText("Привет!" + text)
+
+# ui.progressBar.setValue(0)
+
+# ui.pushButton.clicked.connect(inp)
+
+app.exec_()
